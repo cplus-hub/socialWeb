8259c1f812bd2734ba932b1ee38ab5782c9fac84
吳勖群 <fvgbhn321597@yahoo.com.tw>
2023-06-16 00:26:30 +0800

    feat. login建置

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="./css/login.css">
+    <script src="./js/login.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+    <title>login</title>
+</head>
+
+<body>
+    <div class="main">
+        <form action="" method="post">
+            <input type="text" name="account" class="account" placeholder="Account">
+            <br />
+            <input type="text" name="password" class="password" placeholder="Password">
+            <br />
+            <input type="submit" class="signin" value="Sign in">
+        </form>
+        <div></div>
+    </div>
+</body>
+
+</html>
