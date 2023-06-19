33157e00fcff6266625915d254f683faa84b559d
吳勖群 <fvgbhn321597@yahoo.com.tw>
2023-06-20 00:51:59 +0800

    feat. 新增主頁面

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>socialweb</title>
+    <link rel="stylesheet" type="text/css" href="./css/index.css">
+    <script src="./js/index.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+</head>
+
+<body>
+    <div class="main">
+        <div class="top">
+            <div> <img src="./img/logo_1.png" class="logo" alt="logo"></div>
+            <!-- <form action="" method="post"> -->
+            <input type="search" id="search" name="search" class="search" placeholder="Search...">
+            <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" fill="currentColor" class="bi bi-search">
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+            </svg>
+            <!-- <div><img src="./img/user.jpg" class="top_user"></div> -->
+        </div>
+        <div class="content">
+            <div class="content_1">
+                <div class="menu">首頁</div>
+                <div class="menu">USER</div>
+                <br />
+                <hr style="width:70%;height:1.3px;border:none;background:white;">
+                <div class="menu">貼文</div>
+                <div class="menu">訊息</div>
+                <div class="menu">社團</div>
+                <br />
+                <hr style="width:70%;height:1.3px;border:none;background:white;">
+                <div class="menu">所有看板</div>
+                <div class="menu">軟體工程師</div>
+                <div class="menu">設計</div>
+                <div class="menu">公仔</div>
+                <br />
+                <hr style="width:70%;height:1.3px;border:none;background:white;">
+                <div class="menu">我的最愛</div>
+                <div class="menu">php</div>
+
+                <div></div>
+
+
+            </div>
+            <div class="content_2">
+                <div class="content2_top">
+
+                </div>
+                <div class="content2_content"></div>
+            </div>
+            <div class="content_3">
+                <div>
+                    <div class="contact">聯絡人<img src="./img/search.png" style="width: 10px; height:10px; margin-left:10px;"></div>
+                    <hr style="width:100%;height:1.3px;border:none;background:white;">
+                    <div class="contact_user">小王</div>
+                    <div class="contact_user">小王</div>
+                    <div class="contact_user">小王</div>
+                    <div class="contact_user">小王</div>
+                    <div class="contact_user">小王</div>
+                    <div class="contact_user">小王</div>
+                    <div class="contact_user">小王</div>
+                    <div class="contact_user">小王</div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
